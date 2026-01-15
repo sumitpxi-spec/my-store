31deb47e60e1243156124f4a7eeb6c543907de43
sumitpxi-spec <sumitpxi@gmail.com>
2026-01-16 00:26:15 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import ProductCard from "@/components/ProductCard";
 
-export default async function Home() {
-  const products = await fetch("http://localhost:3000/api/products", {
-    cache: "no-store",
-  }).then(res => res.json());
+const products = await fetch("/api/products", {
+  cache: "no-store",
+}).then(res => res.json());
+
 
   return (
     <div style={{ display: "grid", gap: 20 }}>
@@ -13,3 +13,4 @@ export default async function Home() {
     </div>
   );
 }
+
