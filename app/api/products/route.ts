be43f65356ee9636e55d394fbfd85665bf8f5ab0
sumitpxi-spec <sumitpxi@gmail.com>
2026-01-17 02:21:38 +0530

    Update route.ts

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,7 +1,10 @@
+export const dynamic = "force-dynamic";
+
 import { connectDB } from "@/lib/db";
 import Product from "@/models/Product";
 
 export async function GET() {
   await connectDB();
-  return Response.json(await Product.find());
+  const products = await Product.find();
+  return Response.json(products);
 }
