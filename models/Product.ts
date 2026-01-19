ddc408a5c3496058b1c2b1e5d4d3e36af69790d1
sumitpxi-spec <sumitpxi@gmail.com>
2026-01-19 15:20:44 +0530

    Update Product.ts

--- models/Product.ts
+++ models/Product.ts
@@ -1,13 +1,20 @@
 import mongoose from "mongoose";
 
-const ProductSchema = new mongoose.Schema({
-  title: String,
-  slug: String,
-  description: String,
-  price: Number,
-  images: [String],
-  stock: Number,
-}, { timestamps: true });
+const ProductSchema = new mongoose.Schema(
+  {
+    title: String,
+    slug: String,
+    description: String,
+    price: Number,
+    images: [String],
+    category: [String],
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.models.Product ||
   mongoose.model("Product", ProductSchema);
