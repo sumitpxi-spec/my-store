bb61be073e136e6b6fc5d9814d7ebbc402e0e9fd
sumitpxi-spec <sumitpxi@gmail.com>
2026-01-19 17:20:49 +0530

    Update route.ts

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,17 +1,22 @@
 import { NextResponse } from "next/server";
+import dbConnect from "@/lib/db";
+import Product from "@/models/Product";
 
 export async function GET() {
-  try {
-    const res = await fetch(
-      "https://admin-backend-npfj.onrender.com/api/products",
-      { cache: "no-store" }
-    );
+  await dbConnect();
 
-    const data = await res.json();
+  const products = await Product.find(
+    { active: true },
+    {
+      title: 1,
+      slug: 1,
+      genericName: 1,
+      image: 1,
+      category: 1,
+      isBestSeller: 1,
+      pricePerPill: 1,
+    }
+  ).sort({ createdAt: -1 });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    console.error("API ERROR:", error);
-    return NextResponse.json([], { status: 500 });
-  }
+  return NextResponse.json(products);
 }
